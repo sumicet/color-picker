40b66283f97be2934c30a662d0fad009a05fff17
Cristina <mihailescuc@outlook.com>
2022-06-19 18:37:06 +0300

    Update square to render fixed values correctly

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,12 +1,28 @@
-import { Box, BoxProps, Flex } from '@chakra-ui/layout';
+import { FlexProps, Flex } from '@chakra-ui/layout';
 
-export interface SquareProps extends BoxProps {
+export interface SquareProps extends FlexProps {
     size?: string | number;
 }
 
+/**
+ * Extends Flex
+ * @size Accepts percentages too
+ */
 export function Square({ size = '100%', children, ...props }: SquareProps) {
+    // For fixed values: either a number or a value in px/em/rem etc.
+    if (typeof size === 'number' || (typeof size === 'string' && !size.includes('%'))) {
+        return (
+            <Flex {...props} width={size} minWidth={size} height={size}>
+                {children}
+            </Flex>
+        );
+    }
+
+    // This code doesn't take the full width/height provided by size if size is a fixed value
+    // But it works well for percentages
+    // TODO: Test if this works for vh, vw, etc.
     return (
-        <Box
+        <Flex
             {...props}
             width={size}
             _before={{ content: "''", display: 'block', paddingTop: '100%' }}
@@ -15,6 +31,6 @@ export function Square({ size = '100%', children, ...props }: SquareProps) {
             <Flex position='absolute' top='0' width='100%' height='100%'>
                 {children}
             </Flex>
-        </Box>
+        </Flex>
     );
 }
